3c8ad28fe46339b52db4c940fe7c0216d495bc27
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2026-02-16 22:54:35 +0100

    Add tests

---
+++ tests/utils/test_extra_dependencies.py
@@ -0,0 +1,26 @@
+import pytest
+from pytest_mock import MockerFixture
+
+from wirio._utils._extra_dependencies import ExtraDependencies
+
+
+class TestExtraDependencies:
+    def test_fail_importing_fastapi_when_not_installed(
+        self, mocker: MockerFixture
+    ) -> None:
+        mocker.patch.dict("sys.modules", {"fastapi": None})
+
+        with pytest.raises(ImportError) as exception_info:
+            assert (
+                str(exception_info) == ExtraDependencies.ensure_fastapi_is_installed()
+            )
+
+    def test_fail_importing_sqlmodel_when_not_installed(
+        self, mocker: MockerFixture
+    ) -> None:
+        mocker.patch.dict("sys.modules", {"sqlmodel": None, "greenlet": None})
+
+        with pytest.raises(ImportError) as exception_info:
+            assert (
+                str(exception_info) == ExtraDependencies.ensure_sqlmodel_is_installed()
+            )
