8fce56c560adcee976ed7b5ecc5ce4ffdbb0c558
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2026-02-23 06:49:21 +0100

    Always start examples with FastAPI integration

--- docs/code/getting_started/quickstart/fastapi_full_code.py
+++ docs/code/getting_started/quickstart/fastapi_full_code.py
@@ -29,6 +29,6 @@ async def create_user(
 
 
 services = ServiceCollection()
+services.configure_fastapi(app)
 services.add_transient(EmailService)
 services.add_transient(UserService)
-services.configure_fastapi(app)
